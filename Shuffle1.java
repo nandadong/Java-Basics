4e8659e71d1471b09bc6e26d1c8753e7821f5ad5
Dong Nan <raitodong@gmail.com>
2016-02-29 15:19:00 -0500

    fixed

--- Shuffle1.java
+++ Shuffle1.java
@@ -1,5 +1,5 @@
 class Shuffle1 {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		int x = 3;
 		while (x > 0) {
 			if (x > 2) {
@@ -15,6 +15,5 @@ class Shuffle1 {
 				x = x - 1;
 			}
 		}
-		System.out.println();
 	}
 }
