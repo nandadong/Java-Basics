94961e16cafefa7ef8df81cfe7c49fc53544b713
Dong Nan <raitodong@gmail.com>
2016-06-27 20:55:49 -0400

    simple game

---
+++ NimGame.java
@@ -0,0 +1,12 @@
+public class NimGame {
+    public static boolean canWinNim(int n) {
+    	if(n%4 == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+
+
