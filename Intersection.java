98af06c9bf09b1d8d51b60689f59868c4ab82646
Dong Nan <raitodong@gmail.com>
2016-06-29 18:09:29 -0400

    Used Hashset

--- Intersection.java
+++ Intersection.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class Intersection {
 	public static void main(String[] args) {
-		int[] nums1 = {1, 2, 2, 1};
-		int[] nums2 = {2, 2};
+		int[] nums1 = {};
+		int[] nums2 = {};
 		int[] intersect = intersection(nums1, nums2);
 		for (int i = 0; i < intersect.length; i++) {
 			System.out.print(intersect[i] + " ");
@@ -13,27 +13,21 @@ public class Intersection {
 		if (nums1 == null || nums2 == null) {
 			return null;
 		}
-		List<Integer> list = new ArrayList<Integer>();
+		HashSet<Integer> hs = new HashSet<Integer>();
 		for (int i = 0; i < nums1.length; i++) {
-			for (int j = i + 1; j < nums1.length; j++) {
-				if (nums1[i] == nums1[j]) {
-					nums1[j] = Integer.MAX_VALUE;
-				}
-			}
-			// System.out.print(nums1[i] + " ");
+			hs.add(nums1[i]);
 		}
-		// System.out.print("\n");
-		for (int i = 0; i < nums1.length; i++) {
-			for(int j = 0; j < nums2.length; j++) {
-				if(nums1[i] == nums2[j]) {
-					list.add(nums1[i]);
-					break;
-				}
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		for (int i = 0; i < nums2.length; i++) {
+			if (hs.contains(nums2[i])) {
+				al.add(nums2[i]);
+				hs.remove(nums2[i]);
 			}
 		}
-		int[] result = new int[list.size()];
-		for(int i = 0; i < result.length; i++) {
-			result[i] = list.get(i);
+
+		int[] result = new int[al.size()];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = al.get(i);
 		}
 		return result;
 	}
