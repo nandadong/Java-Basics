e24bdcab6821b22fa716011d02e220f46f77d656
Dong Nan <raitodong@gmail.com>
2016-07-20 00:22:41 -0400

    Modified by mirror integer, with no String created

--- Palindrome.java
+++ Palindrome.java
@@ -1,15 +1,18 @@
 public class Palindrome {
 	public static void main(String[] args) {
-		int x = 1233421;
+		int x = 123321;
 		System.out.print(isPalindrome(x) + "\n");
 	}
 	public static boolean isPalindrome(int x) {
-		String s = String.valueOf(x);
-		for (int i = 0; i < s.length() / 2; i++) {
-			if (s.charAt(i) != s.charAt(s.length()-1-i)) {
-				return false;
-			}
+		if (x < 0) {
+			return false;
 		}
-		return true;
+		int t = x;
+		int res = 0;
+		while (t > 0) {
+			res = res * 10 + t % 10;
+			t = t / 10;
+		}
+		return res == x;
 	}
 }
