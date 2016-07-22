6628599a747b6e44b20020419b12151e3992bcaf
Dong Nan <raitodong@gmail.com>
2016-07-21 21:33:44 -0400

    O(n) time complexity, complete in 1 loop

--- BullsandCows.java
+++ BullsandCows.java
@@ -1,28 +1,22 @@
 import java.util.*;
 public class BullsandCows {
 	public static void main(String[] args) {
-		System.out.print(getHint("1123","0111") + "\n");
+		System.out.print(getHint("1807","7810") + "\n");
 	}
 	public static String getHint(String secret, String guess) {
 		int A = 0, B = 0, len = secret.length();
+		int[] count = new int[10];
 		for (int i = 0; i < len; i++) {
 			if (secret.charAt(i) == guess.charAt(i)) {
-				secret = secret.substring(0,i) + secret.substring(i+1);
-				guess = guess.substring(0,i) + guess.substring(i+1);
-				i --;
-				len --;
 				A ++;
-			}
-		}
-		for (int i = 0; i < len; i++) {
-			for (int j = 0; j < len; j++) {
-				if (secret.charAt(i) == guess.charAt(j)) {
-					secret = secret.substring(0,i) + secret.substring(i+1);
-					guess = guess.substring(0,j) + guess.substring(j+1);
-					i --;
-					len --;
+			} else {
+				count[secret.charAt(i) - '0'] ++;
+				if (count[secret.charAt(i) - '0'] <= 0) {
 					B ++;
-					break;
+				}
+				count[guess.charAt(i) - '0'] --;
+				if (count[guess.charAt(i) - '0'] >= 0) {
+					B ++;
 				}
 			}
 		}
