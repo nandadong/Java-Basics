bde3876f0f97ee6453fc43a4124d5369201d2bca
Dong Nan <raitodong@gmail.com>
2016-08-03 15:40:29 -0400

    mark, classic binary search problem. attention to line 9

---
+++ VersionControl.java
@@ -0,0 +1,22 @@
+public class VersionControl {
+	public static void main(String[] args) {
+		int n = 4;
+		System.out.println(firstBadVersion(n));
+	}
+	public static int firstBadVersion(int n) {
+		int start = 1, end = n;
+		while (start + 1 < end) {
+			int mid = start + (end - start)/2; //in case of overflow
+			if (isBadVersion(mid)) end = mid;
+			else start = mid;
+		}
+		if (isBadVersion(start)) return start;
+		return end;
+	}
+	public static boolean isBadVersion(int version) {
+		if (version >= 4) {
+			return true;
+		}
+		return false;
+	}
+}
