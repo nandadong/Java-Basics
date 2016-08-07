bd589bda09770f060509ea50174c60de9bbcda4d
Dong Nan <raitodong@gmail.com>
2016-08-06 22:22:40 -0400

    single pass, just compare i and i-1

---
+++ BuyandSellStockII.java
@@ -0,0 +1,12 @@
+public class BuyandSellStockII {
+	public static void main(String[] args) {
+		int[] prices = {0, 1, 2, 3, 4, 5};
+		System.out.println(maxProfit(prices));
+	}
+	public static int maxProfit(int[] prices) {
+		int profit = 0;
+		for (int i = 1; i < prices.length; i++)
+			if (prices[i] > prices[i-1]) profit += prices[i] - prices[i-1];
+		return profit;
+	}
+}
