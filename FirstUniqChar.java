428c69d15a9d40679e3485990e3af9ddbeeea487
Dong Nan <raitodong@gmail.com>
2016-08-22 18:22:46 -0400

    frequency map instead of HashMap

---
+++ FirstUniqChar.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FirstUniqChar {
+	public static void main(String[] args) {
+		String s = "llddttee";
+		System.out.println(firstUniqChar(s));
+	}
+	public static int firstUniqChar(String s) {
+		int[] freq = new int[26];
+		for (int i = 0; i < s.length(); i ++) {
+			freq[s.charAt(i) - 'a'] ++;
+		}
+		for (int i = 0; i < s.length(); i ++) {
+			if (freq[s.charAt(i) - 'a'] == 1) return i;
+		}
+		return -1;
+	}
+}
