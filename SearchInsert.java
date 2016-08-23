fd204a8ab7e221298d811b6425bb4214fb6c183d
Dong Nan <raitodong@gmail.com>
2016-08-23 16:02:35 -0400

    AC

---
+++ SearchInsert.java
@@ -0,0 +1,15 @@
+public class SearchInsert {
+	public static void main(String[] args) {
+		int[] nums = {1, 3, 5, 6};
+		int target = 7;
+		System.out.println(searchInsert(nums, target));
+	}
+	public static int searchInsert(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i ++) {
+			if (nums[i] >= target) {
+				return i;
+			}
+		}
+		return nums.length;
+	}
+}
