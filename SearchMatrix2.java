7865a5f9322a7a1798c6b99a904d74aa52f1563b
Dong Nan <raitodong@gmail.com>
2016-08-26 22:43:52 -0400

    O(m+n), starting point matters

---
+++ SearchMatrix2.java
@@ -0,0 +1,22 @@
+public class SearchMatrix2 {
+	public static void main(String[] args) {
+		int[][] matrix = {
+			{1,   4,  7, 11, 15},
+			{2,   5,  8, 12, 19},
+			{3,   6,  9, 16, 22},
+			{10, 13, 14, 17, 24},
+			{18, 21, 23, 26, 30}
+		};
+		int target = 20;
+		System.out.println(searchMatrix(matrix, target));
+	}
+	public static boolean searchMatrix(int[][] matrix, int target) {
+		int row = 0, col = matrix[0].length - 1;
+		while (row < matrix.length && col >= 0) {
+			if (matrix[row][col] == target) return true;
+			if (matrix[row][col] < target) row ++;
+			else col --;
+		}
+		return false;
+	}
+}
