298a3c1b0f9dd3a8f194ce564e3d72ce081afa7e
Dong Nan <raitodong@gmail.com>
2016-09-04 23:41:15 -0400

    mark, top-down then bottom up

---
+++ SetMatrixZeroes.java
@@ -0,0 +1,23 @@
+public class SetMatrixZeroes {
+	public void setZeroes(int[][] matrix) {
+        //top down
+        if (matrix == null || matrix.length == 0) return;
+        int col0 = 1, m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; i ++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < n; j ++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        //bottom up
+        for (int i = m - 1; i >= 0; i --) {
+            for (int j = n - 1; j > 0; j --) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+    }	
+}
